f841de23dd9c2e5e512091c3c989d4206c0ad121
novaariyanto <bajangsampoerna@gmail.com>
2019-11-20 10:21:13 +0700

    ds

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <style> body{ font-family: calibri; }</style>
 <form action='api.php' method='post'> 
-search : <input type='text' name='keyword' value="<?php if(isset($_POST['keyword'])) {echo $_POST['keyword'];}?>"/> 
-<input type='submit' value='cari'/> 
+teks : 
+<br>
+<textarea name="keyword" id="" cols="30" rows="3"></textarea>
+<!-- <input type='text' name='keyword' value="<?php if(isset($_POST['keyword'])) {echo $_POST['keyword'];}?>"/>  -->
+<br>
+<br>
+<input type='submit' value='spin'/> 
 </form>
--- koneksi.php
+++ koneksi.php
@@ -1,7 +1,8 @@
 <?php 
-$koneksi = mysqli_connect('localhost','root','','db_spine');
+// $koneksi = mysqli_connect('localhost','root','','db_spine');
+$koneksi = mysqli_connect('us-cdbr-iron-east-05.cleardb.net','b205d7b92eec82','1835684a','heroku_bd069ea2d8217ca');
 if(!$koneksi){
     echo "koneksi gagal";
 }
-
+// CLEARDB_DATABASE_URL: mysql://:@/?reconnect=true
 ?>
